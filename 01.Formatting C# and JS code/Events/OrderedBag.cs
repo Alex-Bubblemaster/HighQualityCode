e64bec2b4b9a5ffe1cf3bafaf6ad84065f880b93
Alex-Bubblemaster <shiatzu@gmail.com>
2015-07-04 11:14:25 +0300

    C# and JS formatting code

---
+++ 01.Formatting C# and JS code/Events/OrderedBag.cs
@@ -0,0 +1,13 @@
+﻿namespace Events
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class OrderedBag<T>: List<Event>
+    {
+        internal object RangeFrom(Event p1, bool p2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
